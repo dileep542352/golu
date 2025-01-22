18b06a42013d50da39ea192f73df18f61e6e0ef1
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-01-22 09:38:07 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,18 +1,14 @@
 import os
 import asyncio
-from telethon import TelegramClient, events, Button
-from telethon.errors import FloodWait
-from pyrogram import Client as PyroClient, filters, enums
-from pyrogram.errors import UsernameNotOccupied
+from pyrogram import Client, filters, enums
+from pyrogram.errors import UsernameNotOccupied, FloodWait
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from config import API_ID, API_HASH, ERROR_MESSAGE
 from database.db import db
 from TechVJ.strings import HELP_TXT
 
 # Initialize the Pyrogram client
-pyro_client = PyroClient("my_bot", api_id=API_ID, api_hash=API_HASH)
-# Initialize the Telethon client
-aman = TelegramClient('session_name', API_ID, API_HASH)
+pyro_client = Client("my_bot", api_id=API_ID, api_hash=API_HASH)
 
 # Global variables for batch processing
 ids = []
@@ -54,7 +50,7 @@ def progress(current, total, message, type):
         fileup.write(f"{current * 100 / total:.1f}%")
 
 @pyro_client.on_message(filters.command(["start"]))
-async def send_start(client: PyroClient, message: Message):
+async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
@@ -72,14 +68,14 @@ async def send_start(client: PyroClient, message: Message):
     )
 
 @pyro_client.on_message(filters.command(["help"]))
-async def send_help(client: PyroClient, message: Message):
+async def send_help(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"{HELP_TXT}"
     )
 
 @pyro_client.on_message(filters.command(["cancel"]))
-async def send_cancel(client: PyroClient, message: Message):
+async def send_cancel(client: Client, message: Message):
     batch_temp.IS_BATCH[message.from_user.id] = True
     await client.send_message(
         chat_id=message.chat.id,
@@ -87,7 +83,7 @@ async def send_cancel(client: PyroClient, message: Message):
     )
 
 @pyro_client.on_message(filters.text & filters.private)
-async def save(client: PyroClient, message: Message):
+async def save(client: Client, message: Message):
     if "https://t.me/" in message.text:
         if batch_temp.IS_BATCH.get(message.from_user.id) == False:
             return await message.reply_text("**One Task Is Already Processing. Wait For Complete It. If You Want To Cancel This Task Then Use - /cancel**")
@@ -107,7 +103,7 @@ async def save(client: PyroClient, message: Message):
                 batch_temp.IS_BATCH[message.from_user.id] = True
                 return
             try:
-                acc = PyroClient("saverestricted", session_string=user_data, api_hash=API_HASH, api_id=API_ID)
+                acc = Client("saverestricted", session_string=user_data, api_hash=API_HASH, api_id=API_ID)
                 await acc.connect()
             except:
                 batch_temp.IS_BATCH[message.from_user.id] = True
@@ -149,7 +145,7 @@ async def save(client: PyroClient, message: Message):
             await asyncio.sleep(3)
         batch_temp.IS_BATCH[message.from_user.id] = True
 
-async def handle_private(client: PyroClient, acc, message: Message, chatid: int, msgid: int):
+async def handle_private(client: Client, acc, message: Message, chatid: int, msgid: int):
     msg: Message = await acc.get_messages(chatid, msgid)
     if msg.empty: return
     msg_type = get_message_type(msg)
@@ -291,125 +287,5 @@ def get_message_type(msg: Message):
     except:
         pass
 
-@aman.on(events.NewMessage(incoming=True, pattern='/batch'))
-async def _batch(event):
-    s = False
-    if f'{event.sender_id}' in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
-    async with aman.conversation(event.chat_id) as conv:
-        if not s:
-            await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
-            try:
-                link = await conv.get_reply()
-                try:
-                    _link = get_link(link.text)
-                except Exception:
-                    await conv.send_message("No link found.")
-            except Exception as e:
-                return await conv.send_message("Cannot wait more longer for your response!")
-            await conv.send_message("Send me the number of files/range you want to save from the given message, as a reply to this message.", buttons=Button.force_reply())
-            try:
-                _range = await conv.get_reply()
-            except Exception as e:
-                return await conv.send_message("Cannot wait more longer for your response!")
-            try:
-                value = int(_range.text)
-                if value > 1000000:
-                    return await conv.send_message("You can only get up to 100000 files in a single batch.")
-            except ValueError:
-                return await conv.send_message("Range must be an integer!")
-            for i in range(value):
-                ids.append(i)
-            s, r = await check(aman, _link)
-            if s != True:
-                await conv.send_message(r)
-                return
-            batch.append(f'{event.sender_id}')
-            cd = await conv.send_message("**Batch process ongoing...**\n\nProcess completed: ",
-                                         buttons=[[Button.url("Join Channel", url="http://t.me/LegendBotzs")]])
-            co = await run_batch(aman, event.sender_id, cd, _link)
-            try:
-                if co == -2:
-                    await aman.send_message(event.sender_id, "Batch successfully completed!")
-                    await cd.edit(f"**Batch process ongoing.**\n\nProcess completed: {value} \n\n Batch successfully completed! ")
-            except:
-                await aman.send_message(event.sender_id, "ERROR!\n\n maybe last msg didn't exist yet")
-            conv.cancel()
-            ids.clear()
-            batch.clear()
-
-@aman.on(events.callbackquery.CallbackQuery(data="cancel"))
-async def cancel(event):
-    ids.clear()
-    batch.clear()
-
-async def run_batch(userbot, sender, countdown, link):
-    for i in range(len(ids)):
-        timer = 6
-        if i < 250:
-            timer = 2
-        elif i < 1000:
-            timer = 3
-        elif i < 10000:
-            timer = 4
-        elif i < 50000:
-            timer = 5
-        elif i < 100000:
-            timer = 6
-        elif i < 200000:
-            timer = 8
-        elif i < 1000000:
-            timer = 10
-
-        if 't.me/c/' not in link:
-            timer = 1 if i < 500 else 2
-        try:
-            count_down = f"**Batch process ongoing.**\n\nProcess completed: {i + 1}"
-            msg_id = int(link.split("/")[-1]) + int(ids[i])
-            await get_bulk_msg(userbot, sender, link, msg_id)
-
-            protection = await userbot.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
-            await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="https://t.me/LegendBotzs")]])
-            await asyncio.sleep(timer)
-            await protection.delete()
-
-        except IndexError:
-            await userbot.send_message(sender, f"Batch ended completed!")
-            await countdown.delete()
-            break
-
-        except FloodWait as fw:
-            if int(fw.value) > 300:
-                await userbot.send_message(sender, f'You have floodwaits of {fw.value} seconds, cancelling batch')
-                ids.clear()
-                break
-            else:
-                fw_alert = await userbot.send_message(sender, f'Sleeping for {fw.value + 5} second(s) due to telegram floodwait.')
-                await asyncio.sleep(fw.value + 5)
-                await fw_alert.delete()
-
-        except Exception as e:
-            await userbot.send_message(sender, f"An error occurred during cloning, batch will continue.\n\n**Error:** {str(e)}")
-
-        n = i + 1
-        if n == len(ids):
-            return -2
-
-C = "/cancel"
-START_PIC = "https://telegra.ph/file/c37f3eaf3e59e7e64fde7.png"
-TEXT = "👋 Hi, This is 'Paid Restricted Content Saver' bot Made with ❤️ by __**Legend Union**__."
-
-@aman.on(events.NewMessage(pattern=f"^{C}"))
-async def start_command(event):
-    buttons = [
-        [Button.inline("Cancel", data="cancel"),
-         Button.inline("Cancel", data="cancel")],
-        [Button.url("Join Channel", url="https://telegram.dog/LegendBotzs")]
-    ]
-
-    await aman.send_file(
-        event.chat_id,
-        file=START_PIC,
-        caption=TEXT,
-        buttons=buttons
-    )
+# Start the Pyrogram client
+pyro_client.run()
