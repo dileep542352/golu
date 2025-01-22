b4f397f4f7a0bd316609a53467ab7af035ea7fa6
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-01-22 05:57:46 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -30,6 +30,23 @@ https://t.me/c/xxxx/101 - 120
 ```
 
 __note that space in between doesn't matter__"""
+
+**__BATCH COMMAND__**
+
+The `/batch` command allows you to start saving messages from a specified link. You need to provide the message link and the number of messages to save.
+
+- **Usage:**
+    1. Send `/batch` to initiate the process.
+    2. Provide the message link when prompted.
+    3. Specify the number of messages you want to save.
+
+__Please ensure you use this command when you are sure, as cancellation might not be available.__
+
+**__CANCEL BATCH__**
+
+If you wish to cancel an ongoing batch process, use the `/cancel` command.
+
+__Ensure the bot is a member of the groups or channels from which you want to batch save messages.__
        
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
